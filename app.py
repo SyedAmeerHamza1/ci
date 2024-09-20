98f8a43283098f0c29de8ad9f82c6bfc8a8ef79c
Syed Ameer Hamza <Syedsohail116@gmail.com>
2024-09-20 17:48:02 +0530

    again fixed flake8

--- app.py
+++ app.py
@@ -6,11 +6,11 @@ def sub(a, b):
     return a-b
 
 
-if __name__== "__main__":
+if __name__ == "__main__":
 
-    result1= add(5, 5)
+    result1 = add(5, 5)
 
-    result2= sub(5, 2)
+    result2 = sub(5, 2)
 
     print(result1, result2)
-    
+
