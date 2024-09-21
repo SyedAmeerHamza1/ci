aafbf12cbc7f198763ed84c25a075385de3541bb
Syed Ameer Hamza <Syedsohail116@gmail.com>
2024-09-21 19:23:50 +0530

    added unit testing

--- app.py
+++ app.py
@@ -6,10 +6,16 @@ def sub(a, b):
     return a-b
 
 
+def mul(a, b):
+    return a*b
+
+
 if __name__ == "__main__":
 
     result1 = add(5, 5)
 
     result2 = sub(5, 2)
+
+    result3 = mul(2, 2)
 
     print(result1, result2)
